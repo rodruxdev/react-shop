2af63990faa8fb42e4506d43466f7fa2942bf0ff
rodrigojgq <rodrigojgq@gmail.com>
2022-02-08 19:12:09 -0400

    ✨ [ADD] Login and layout components
    Add styles in login component.
    Change config for css and scss files in webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import '../styles/global.scss';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 const App = () => {
-  return <h1>Hola mundo, Bienvenidos al Cuso Práctico de React!</h1>;
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
 };
 
 export default App;
