28aa242954a5d3af0712fc15414f28948a8a447f
Rodrigo Goitia <53325125+rodruxdev@users.noreply.github.com>
2022-02-07 23:52:01 -0400

    🔧 [ADD]  Initial config (#1)
    * 🔧 [ADD] Webpack and babel config
    
    * 🔧 [ADD] Finish server configs
    
    Add structure to html.
    Add import in index.
    Changes in App component
    
    * 🔧 [ADD] Webpack styles config
    
    Add a global styles file.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import '../styles/global.scss';
 
 const App = () => {
-  return 
-  <h1>Hola mundo!</h1>;
+  return <h1>Hola mundo, Bienvenidos al Cuso Práctico de React!</h1>;
 };
 
 export default App;
