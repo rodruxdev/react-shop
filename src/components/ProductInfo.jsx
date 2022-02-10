fcb13ce0ed9a47834ab4cf97c9ca8f86fad2982b
rodrigojgq <rodrigojgq@gmail.com>
2022-02-09 20:47:26 -0400

    ✨[ADD] useState for toggle menu and cart

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -3,13 +3,19 @@ import '@styles/ProductInfo.scss';
 import addCart from '@icons/bt_add_to_cart.svg';
 
 const ProductInfo = () => {
+  const [cart, setCart] = React.useState([]);
+
+  const handleClick = () => {
+    setCart([])
+  }
+
   return (
     <div className="ProductInfo">
       <div>
         <p>$120,00</p>
         <p>Bike</p>
       </div>
-      <figure>
+      <figure onClick={handleClick}>
         <img src={addCart} alt="" />
       </figure>
     </div>
