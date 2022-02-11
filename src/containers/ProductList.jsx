97915b8d87425066bfe1123b5ec9477e9ae7aab0
rodrigojgq <rodrigojgq@gmail.com>
2022-02-11 16:13:35 -0400

    ✨[ADD] API conection to products with useEffect

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 import ProductItem from '@containers/ProductItem';
 import '@styles/ProductList.scss';
 
+const API = 'https://api.escuelajs.co/api/v1/products';
+
 const ProductList = () => {
+	const [products, setProducts] = React.useState([]);
+
+	React.useEffect(async () => {
+		const response = await axios(API);
+		setProducts(response.data);
+	}, [])
 	return (
 		<section className="main-container">
 			<div className="ProductList">
-				<ProductItem />
+				{products.map(product => (
+					<ProductItem />
+				))}
 			</div>
 		</section>
 	);
